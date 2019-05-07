44641f062ee670a88b8ae5c7127975f36d47386b
alexander.zhulinsky <alzhulinsky@gmail.com>
2019-05-08 00:46:02 +0300

    Swagger WIP

--- src/main/java/ua/com/foxminded/hotelbooking/controller/OptionController.java
+++ src/main/java/ua/com/foxminded/hotelbooking/controller/OptionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -15,18 +16,19 @@ import io.swagger.annotations.ApiResponses;
 import ua.com.foxminded.hotelbooking.domain.Option;
 import ua.com.foxminded.hotelbooking.repository.OptionRepository;
 
-@RestController("/options")
+@RestController
+@RequestMapping("/options")
 @Api(value = "Options")
 public class OptionController {
 
     @Autowired
     private OptionRepository optionRepository;
     
-    @GetMapping
     @ApiOperation(value = "Get list of available options", response = List.class)
     @ApiResponses(value = { 
     		@ApiResponse(code = 200, message = "Successfully retrieved list"),
     		@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")})
+    @GetMapping
     public ResponseEntity<Iterable<Option>> getAllOptions() {
         return new ResponseEntity<>(optionRepository.findAll(), HttpStatus.OK);
     }
--- src/main/java/ua/com/foxminded/hotelbooking/controller/UserController.java
+++ src/main/java/ua/com/foxminded/hotelbooking/controller/UserController.java
@@ -10,17 +10,25 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import ua.com.foxminded.hotelbooking.domain.User;
 import ua.com.foxminded.hotelbooking.repository.UserRepository;
 
 @RestController
+@RequestMapping("/users")
+@Api(value = "User")
 public class UserController {
     
     @Autowired
@@ -33,20 +41,38 @@ public class UserController {
         }
     }
     
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    @ApiOperation(value = "View a list of available users")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retrieves all available users"),
+            @ApiResponse(code = 500, message = "Server Error")
+    })
+    @GetMapping
     public ResponseEntity<Iterable<User>> getAllUsers() {
         return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);        
     }
     
-    @RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
-    public ResponseEntity<?> getUser(@PathVariable Long userId) {
+    @ApiOperation(value = "Retrieves an existing User")
+    @ApiResponses(value = { 
+            @ApiResponse(code = 200, message = "Body: User data"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Server Error")
+    })
+    @GetMapping("{userId}")
+    public ResponseEntity<?> getUser(
+            @ApiParam(value = "User id from which user object will retrieve", required = true) @PathVariable Long userId) {
         verifyUser(userId); 
         Optional<User> user = userRepository.findById(userId);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
     
-    @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
+    @ApiOperation(value = "Create new User")
+    @ApiResponses(value = { 
+            @ApiResponse(code = 201, message = "Newly created user URI"),
+            @ApiResponse(code = 500, message = "Server Error")
+    })
+    @PostMapping
+    public ResponseEntity<?> createUser(
+            @ApiParam(value = "User object to store in database table", required = true) @Valid @RequestBody User user) {
         user = userRepository.save(user);
         // Set the location header for the newly created resource
         HttpHeaders responseHeaders = new HttpHeaders();
