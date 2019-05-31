1247586fe5f14d1679c5d6f2d7c32fd3efe0e727
Aljuly <alg252@yandex.ru>
2019-05-31 18:03:59 +0300

    Forgotten

--- src/main/java/ua/com/foxminded/hotelbooking/controller/RoomController.java
+++ src/main/java/ua/com/foxminded/hotelbooking/controller/RoomController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import ua.com.foxminded.hotelbooking.domain.Room;
@@ -22,24 +23,26 @@ public class RoomController {
 	@Autowired
 	private RoomRepository roomRepository;
 	
-    @ApiOperation(value = "Retrieves an existing User")
+    @ApiOperation(value = "Retrieves all available roms \r\n" + 
+    		"for a given date range")
     @ApiResponses(value = { 
-            @ApiResponse(code = 200, message = "Body: User data"),
+            @ApiResponse(code = 200, message = "Body:Room List"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
             @ApiResponse(code = 500, message = "Server Error")
     })
 	@GetMapping("/rooms/dates/{from}/{to}")
 	public ResponseEntity<?> getRoomByDates(
-			@PathVariable("from") @DateTimeFormat(pattern = "yyyy-MM-dd") Date checkIn,
-			@PathVariable("to") @DateTimeFormat(pattern = "yyyy-MM-dd") Date ckeckOut) {
+			@ApiParam(value = "Room booked from date", required = true) @PathVariable("from") @DateTimeFormat(pattern = "yyyy-MM-dd") Date checkIn,
+			@ApiParam(value = "User id from which user object will retrieve", required = true) @PathVariable("to") @DateTimeFormat(pattern = "yyyy-MM-dd") Date ckeckOut) {
 		Iterable<Room> allrooms = roomRepository.findByDate(checkIn, ckeckOut);
 		
 		return new ResponseEntity<>(allrooms, HttpStatus.OK); 
 	}
 
-    @ApiOperation(value = "Retrieves an existing User")
+    @ApiOperation(value = "Retrieves all available roms \r\n" + 
+    		"of a given category")
     @ApiResponses(value = { 
-            @ApiResponse(code = 200, message = "Body: User data"),
+            @ApiResponse(code = 200, message = "Body:Room List"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
             @ApiResponse(code = 500, message = "Server Error")
     })
