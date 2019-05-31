ec6d2e2e31079de8076696b28533567080d8d02b
Aljuly <alg252@yandex.ru>
2019-05-31 18:03:40 +0300

    WIP

--- src/main/java/ua/com/foxminded/hotelbooking/controller/RoomController.java
+++ src/main/java/ua/com/foxminded/hotelbooking/controller/RoomController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import ua.com.foxminded.hotelbooking.domain.Room;
 import ua.com.foxminded.hotelbooking.repository.RoomRepository;
 
@@ -20,7 +22,13 @@ public class RoomController {
 	@Autowired
 	private RoomRepository roomRepository;
 	
-	@RequestMapping(value = "/rooms/dates/{from}/{to}", method = RequestMethod.GET)
+    @ApiOperation(value = "Retrieves an existing User")
+    @ApiResponses(value = { 
+            @ApiResponse(code = 200, message = "Body: User data"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Server Error")
+    })
+	@GetMapping("/rooms/dates/{from}/{to}")
 	public ResponseEntity<?> getRoomByDates(
 			@PathVariable("from") @DateTimeFormat(pattern = "yyyy-MM-dd") Date checkIn,
 			@PathVariable("to") @DateTimeFormat(pattern = "yyyy-MM-dd") Date ckeckOut) {
@@ -29,7 +37,13 @@ public class RoomController {
 		return new ResponseEntity<>(allrooms, HttpStatus.OK); 
 	}
 
-	@RequestMapping(value = "rooms/{id}", method = RequestMethod.GET)
+    @ApiOperation(value = "Retrieves an existing User")
+    @ApiResponses(value = { 
+            @ApiResponse(code = 200, message = "Body: User data"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Server Error")
+    })
+	@GetMapping("rooms/{id}")
 	public ResponseEntity<?> getRoomsByCategory(@PathVariable("id") Long id) {
 		Iterable<Room> allRooms = roomRepository.findByCategory(id);
 		return new ResponseEntity<>(allRooms, HttpStatus.OK);
